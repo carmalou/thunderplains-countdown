b32ffa7c883ccf69f0382cbd4e1c9a2348c02602
carmalou <carmenbourlonlong@gmail.com>
2018-07-22 22:31:18 -0500

    add in countdown

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,5 @@
 const applicationServerPublicKey = 'BC844Rj7t_QfXN2d44qIXoUTEGHlUAanrIqxdga3Vv-jicYvrAHpx5cncywF7Ammi2PqTyxvHcadAxCg2INNXvo';
+const moment = require('moment');
 
 function urlB64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
@@ -18,41 +19,58 @@ function urlB64ToUint8Array(base64String) {
 if(navigator.serviceWorker) {
     // register sw
 
-    navigator.serviceWorker.register('./../serviceworker.js')
+    navigator.serviceWorker.register('./serviceworker.js')
     .then(function(swReg) {
         if(window.PushManager) {
-            // set up push
-            return swReg.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlB64ToUint8Array(applicationServerPublicKey)
-            })
+            // check if user is subscribed
+            // if they are subscribed, just return
+            // if not, ask to subscribe
+            return swReg.pushManager.getSubscription()
             .then(function(subscription) {
-                return fetch('http://localhost:3000/api/save-subscription/', {
-                    method: "POST", // *GET, POST, PUT, DELETE, etc.
-                    headers: {
-                        "Content-Type": "application/json; charset=utf-8",
-                    },
-                    body: JSON.stringify(subscription), // body data type must match "Content-Type" header
-                })
-                .then(function(response) {
-                    console.log('success ', response);
-                })
-                .catch(function(err) {
-                    console.log('err ', err);
-                })
-            })
-            .catch(function(err) {
-                console.log('something is probs missing ', err);
+                if(!subscription) {
+                    return;
+                } else {
+                    // set up push
+                    return swReg.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: urlB64ToUint8Array(applicationServerPublicKey)
+                    })
+                    .then(function(subscription) {
+                        return fetch('http://localhost:3000/api/save-subscription/', {
+                            method: "POST", // *GET, POST, PUT, DELETE, etc.
+                            headers: {
+                                "Content-Type": "application/json; charset=utf-8",
+                            },
+                            body: JSON.stringify(subscription), // body data type must match "Content-Type" header
+                        })
+                        .then(function(response) {
+                            console.log('success ', response);
+                        })
+                        .catch(function(err) {
+                            console.log('err ', err);
+                        })
+                    })
+                    .catch(function(err) {
+                        console.log('something is probs missing ', err);
+                    })
+                }
             })
         }
     })
-} else {
-    // do nothing
 }
 
 function countdown() {
-    // TODO: make interval for countdown
-    console.log('right now I\'m just a log statement');
+    var tpc = moment('2018-11-01');
+    var rightNow = moment();
+    var diff = tpc.diff(rightNow);
+    var duration = moment.duration(diff);
+
+    var months = duration.get('months');
+    var days = duration.get('days');
+    var hours = duration.get('hours');
+    var seconds = duration.get('seconds');
+
+    document.getElementById('countdown_span').textContent = months + " months " + days + " days  " + hours + " hours and " + seconds + " seconds.";
 }
 
-countdown();
+window.setInterval(countdown, 1000);
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: {
       index: './src/index.js',
-      serviceworker: './src/serviceworker.js'
+      serviceworker: './serviceworker.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
